97641a91f269e0dc95162f672ad4327d1f32f702
Gabriel Valiente <105058637+gsvaliente@users.noreply.github.com>
2024-09-16 15:31:08 +0200

    Basic homepage created

--- app/page.js
+++ app/page.js
@@ -6,13 +6,15 @@ export default function HomePage() {
         backgroundImage: "url(https://img.daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.webp)",
       }}
     >
-      <div className="hero-overlay bg-opacity-60"></div>
+      <div className="hero-overlay bg-opacity-80"></div>
       <div className="hero-content text-neutral-content text-center">
-        <div className="max-w-md">
-          <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+        <div className="max-w-md lg:max-w-lg">
+          <h1 className="mb-5 text-7xl font-bold">Welcome to Traverse</h1>
           <p className="mb-5">
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In
-            deleniti eaque aut repudiandae et a id nisi.
+            Your AI-powered travel companion! Uncover hidden gems and local favorites in any city with our intelligent
+            recommendations. Traverse uses cutting-edge AI to tailor travel tips and information to your unique
+            interests. Just tell us where you are headed, and let our app guide you to unforgettable experiences. Start
+            your journey of discovery today!
           </p>
           <button className="btn btn-primary">Get Started</button>
         </div>
