663a2d5dbfd70bfcdcc491706f2e2cc6cd78ae46
Gabriel Valiente <105058637+gsvaliente@users.noreply.github.com>
2024-09-16 17:50:43 +0200

    Installing Clerk for authentication

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -1,11 +1,14 @@
 import { NavLinks } from "./NavLinks";
 import { SidebarHeader } from "./SidebarHeader";
 
+//TODO add a user section to the sidebar at the bottom of the page
+
 export function Sidebar() {
   return (
     <div className="px-4 w-80 min-h-full bg-base-300 py-12 grid grid-rows-[auto,1fr,auto]">
       <SidebarHeader />
       <NavLinks />
+      Profile will go here
     </div>
   );
 }
