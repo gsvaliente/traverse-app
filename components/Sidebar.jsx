9659cd58503fed09555fc472910bc544b6cbd6b2
Gabriel Valiente <105058637+gsvaliente@users.noreply.github.com>
2024-09-16 22:26:58 +0200

    Created the profile section in the Sidebar

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -1,5 +1,6 @@
 import { NavLinks } from "./NavLinks";
 import { SidebarHeader } from "./SidebarHeader";
+import { UserProfile } from "./UserProfile";
 
 //TODO add a user section to the sidebar at the bottom of the page
 
@@ -8,7 +9,7 @@ export function Sidebar() {
     <div className="px-4 w-80 min-h-full bg-base-300 py-12 grid grid-rows-[auto,1fr,auto]">
       <SidebarHeader />
       <NavLinks />
-      Profile will go here
+      <UserProfile />
     </div>
   );
 }
