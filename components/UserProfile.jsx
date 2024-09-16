ba8a2c8b8f1f9bba06a64b457deac824c2d17eb2
Gabriel Valiente <105058637+gsvaliente@users.noreply.github.com>
2024-09-16 22:27:39 +0200

    Profile route now working with UserProfile component from Clerk

---
+++ components/UserProfile.jsx
@@ -0,0 +1,13 @@
+import { UserButton } from "@clerk/nextjs";
+import { currentUser } from "@clerk/nextjs/server";
+
+export async function UserProfile() {
+  const user = await currentUser();
+  console.log(user.emailAddresses[0].emailAddress);
+  return (
+    <div className="px-4 flex items-center gap-2">
+      <UserButton />
+      <p>{user.emailAddresses[0].emailAddress}</p>
+    </div>
+  );
+}
