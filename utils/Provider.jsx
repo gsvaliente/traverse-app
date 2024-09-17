65880839f202f83d14f0bbcc534b9343523601ac
Gabriel Valiente <105058637+gsvaliente@users.noreply.github.com>
2024-09-17 10:36:38 +0200

    Created Provider component to wrap the app, since it is a client component

--- utils/Provider.jsx
+++ utils/Provider.jsx
@@ -1,5 +1,16 @@
+"use client";
+
+import { useState } from "react";
+
+import { QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
 export function Provider({ children }) {
   const [queryClient] = useState(() => new queryClient());
 
-  return;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false}>{children}</ReactQueryDevtools>
+    </QueryClientProvider>
+  );
 }
