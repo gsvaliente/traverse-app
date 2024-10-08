4e150b43215630e395a505f5e1945a141ce51e57
Gabriel Valiente <105058637+gsvaliente@users.noreply.github.com>
2024-10-08 14:09:05 +0200

    Installed toast package

--- utils/providers.jsx
+++ utils/providers.jsx
@@ -2,6 +2,7 @@
 
 import { QueryClient, QueryClientProvider, isServer } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { Toaster } from "react-hot-toast";
 
 function makeQueryClient() {
   return new QueryClient({
@@ -29,6 +30,7 @@ export function Provider({ children }) {
 
   return (
     <QueryClientProvider client={queryClient}>
+      <Toaster position="top-center" />
       <ReactQueryDevtools initialIsOpen={false} />
       {children}
     </QueryClientProvider>
