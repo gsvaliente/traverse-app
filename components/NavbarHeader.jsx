2f13ce9a20192a1724de8bfe518f2e2b15e09db2
Gabriel Valiente <105058637+gsvaliente@users.noreply.github.com>
2024-10-08 12:51:47 +0200

    Changed logo color to test

--- components/NavbarHeader.jsx
+++ components/NavbarHeader.jsx
@@ -3,8 +3,8 @@ import { FaCampground } from "react-icons/fa6";
 export function NavbarHeader() {
   return (
     <>
-      <FaCampground className="w-8 h-8 text-primary" />
-      <h2 className="text-3xl font-extrabold text-primary mr-auto">Traverse</h2>
+      <FaCampground className="w-8 h-8 text-white" />
+      <h2 className="text-3xl font-extrabold text-white mr-auto">Traverse</h2>
     </>
   );
 }
