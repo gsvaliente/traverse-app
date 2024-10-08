32d6a65150cd3c03ef3031c3a093b0dfe16f1332
Gabriel Valiente <105058637+gsvaliente@users.noreply.github.com>
2024-10-08 21:21:39 +0200

    Created the form that will control the search for the tour in the main tours page

--- components/AllTours.jsx
+++ components/AllTours.jsx
@@ -1,7 +1,42 @@
+"use client";
+
+import { getAllTours } from "@/utils/actions";
+import { useQuery } from "@tanstack/react-query";
+import { useState } from "react";
+
+import { TourList } from "./TourList";
+
 export function AllTours() {
+  const [userSearchQuery, setUserSearchQuery] = useState("");
+
+  const { data, isPending } = useQuery({
+    queryKey: ["tours", ""],
+    queryFn: () => getAllTours(userSearchQuery),
+  });
+
   return (
-    <div>
-      <h1>ALL TOURS HERE</h1>
+    <div className="h-[calc(100vh-7rem)] py-5 my-5 flex flex-col">
+      <form className="max-w-lg mb-12 ">
+        <div className="join w-full">
+          <input
+            type="text"
+            placeholder="enter city or country"
+            className="input input-bordered join-item w-full"
+            value={userSearchQuery}
+            onChange={(e) => setUserSearchQuery(e.target.value)}
+            required
+          />
+          <button
+            type="button"
+            className="btn btn-primary join-item"
+            disabled={isPending}
+            onClick={() => setSearchValue("")}
+          >
+            {isPending ? "please wait" : "reset"}
+          </button>
+        </div>
+      </form>
+      {isPending ? <span className="loading"></span> : <TourList data={data} />}
     </div>
   );
 }
