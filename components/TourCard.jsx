770d4eb1c5fda34beeb286bce12d2f7f5adb6a42
Gabriel Valiente <105058637+gsvaliente@users.noreply.github.com>
2024-10-09 12:19:28 +0200

    Created Style for the TourCard in the TourList

--- components/TourCard.jsx
+++ components/TourCard.jsx
@@ -1,13 +1,24 @@
 import Link from "next/link";
 
 export function TourCard({ tour }) {
-  const { city, id, country, tourType } = tour;
+  const { city, id, country, tourType, title } = tour;
   return (
     <Link href={`/tours/${id}`} className="card card-compact rounded-xl bg-base-100">
-      <div className="card-body items-center text-center">
-        <h2 className="card-title text-center capitalize">
-          {city}, {country}, {tourType}
-        </h2>
+      <div className="card bg-base-100 w-80 shadow-xl">
+        <figure>
+          <img src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp" alt="Shoes" />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title capitalize">
+            {city}, {country}
+            <div className="badge badge-secondary">{tourType}</div>
+          </h2>
+          <p>{title}</p>
+          {/* <div className="card-actions justify-end">
+            <div className="badge badge-outline">Fashion</div>
+            <div className="badge badge-outline">Products</div>
+          </div> */}
+        </div>
       </div>
     </Link>
   );
--- components/TourList.jsx
+++ components/TourList.jsx
@@ -1,10 +1,9 @@
 import { TourCard } from "./TourCard";
 
 export function TourList({ data }) {
-  console.log(data);
   if (data.length === 0) return <h4 className="text-lg">No Tours Were Found...</h4>;
   return (
-    <div className="grid sm:grid-cols-2 lg:grid-cols-4 gap-8">
+    <div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-8">
       {data.map((tour) => {
         return <TourCard key={tour.id} tour={tour} />;
       })}
